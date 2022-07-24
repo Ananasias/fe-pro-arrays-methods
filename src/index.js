9efca00aa009ea4fee9c4e50bbcd25def7d3052d
Anastasia Tierokhina <anastasia.avanheim@gmail.com>
2022-07-24 23:18:39 +0300

    final commit arrays methods

--- src/index.js
+++ src/index.js
@@ -4,56 +4,107 @@ import { goods } from './goods.js';
  * @param {string} brand
  * @returns {*}
  */
-const brandFilter = (brand) => {};
+/** 1. дОЛЖЕН ВЕРНУТЬ ОТФИЛЬТРОВАНИЕ ПО БРЕНДУ. (БРЕНД) - ЭТО СТРОКА. НУЖНО ВЕРНУТЬ ОТФИЛЬТРОВАННЫЙ МАССИВ, В КОТОРОМ БРЕНД = КОНКРЕТНОМУ БРЕНДУ */
+const brandFilter = (brand) => {
+  return goods.filter((item) => item.brand === brand);
+};
 
 /**
  * @param {string} color
  * @returns {*}
  */
-const colorFilter = (color) => {};
+/** ТОЖЕ САМОЕ ЧТО И С БРЕНДОМ */
+const colorFilter = (color) => {
+  return goods.filter((item) => item.color === color);
+};
 
 /**
  * @param {string} model
  * @returns {*}
  */
-const modelFilter = (model) => {};
+/** ТОЖЕ САМОЕ ЧТО И С БРЕНДОМ */
+const modelFilter = (model) => {
+  return goods.filter((item) => item.model === model);
+};
 
 /**
  * @param {number} memory
  * @returns {*}
  */
-const memoryFilter = (memory) => {};
+/** ТОЖЕ САМОЕ ЧТО И С БРЕНДОМ */
+const memoryFilter = (memory) => {
+  return goods.filter((item) => item.memory === memory);
+};
 
 /**
  * @param {number} price
  * @returns {*}
  */
-const priceFilter = (price) => {};
+/** ТОЖЕ САМОЕ ЧТО И С БРЕНДОМ */
+const priceFilter = (price) => {
+  return goods.filter((item) => item.price === price);
+};
 
 /**
  * @param {string} country
  * @returns {*}
  */
-const countryFilter = (country) => {};
+/** ТОЖЕ САМОЕ ЧТО И С БРЕНДОМ */
+const countryFilter = (country) => {
+  return goods.filter((item) => item.country === country);
+};
 
 /**
  * @param {string} os
  * @returns {*}
  */
-const osFilter = (os) => {};
+/** ТОЖЕ САМОЕ ЧТО И С БРЕНДОМ */
+const osFilter = (os) => {
+  return goods.filter((item) => item.os === os);
+};
 
 /**
  * @param {number} from
  * @param {number} to
  */
-const rangeFilter = (from, to) => {};
+/** ФИЛЬТР ТОВАРОВ ОТ И ВКЛЮЧАЯ ИСПОЛЬЗУЯ ФИЛЬТР */
+const rangeFilter = (from, to) => {
+  return goods.filter((item) => item.price >= from && item.price <= to);
+};
 
-const minPriceReducer = () => {};
+/** ЧЕРЕЗ РЕДЮС ДОЛЖНО ВЕРНУТЬ МИНИМАЛЬНОЕ ЧИСЛО*/
+const minPriceReducer = () => {
+  return goods.reduce((acc, current) => Math.min(acc, current.price), Infinity);
+};
 
-const maxPriceReducer = () => {};
+/** ЧЕРЕЗ РЕДЮС МАКСИМАЛЬНОЕ ЧИСЛО */
+const maxPriceReducer = () => {
+  return goods.reduce((acc, current) => Math.max(acc, current.price), 0);
+};
 
-const toMaxSorter = () => {};
-const toMinSorter = () => {};
+/** СОРТИРОВКА ОТ БОЛЬШЕГО К МЕНЬШЕМУ ЧЕРЕЗ СОРТ*/
+const toMaxSorter = () => {
+  return goods.sort((a, b) => {
+    if (a.price < b.price) return 1;
+    if (a.price > b.price) return -1;
+    return 0;
+  });
+
+  // второй вариант return goods.sort((a, b) => b.price - a.price);
+
+};
+
+/** СОРТИРОВКА ОТ МЕНЬШЕГО К БОЛЬШЕМУ*/
+const toMinSorter = () => {
+  return goods.sort((a, b) => {
+    if (a.price > b.price) return 1;
+    if (a.price < b.price) return -1;
+    return 0;
+  });
+
+  // второй вариант return goods.sort((a, b) => a.price - b.price)
+
+};
 
 export const filters = {
   brandFilter,
@@ -63,15 +114,15 @@ export const filters = {
   colorFilter,
   memoryFilter,
   modelFilter,
-  rangeFilter,
+  rangeFilter
 };
 
 export const reducers = {
   minPriceReducer,
-  maxPriceReducer,
+  maxPriceReducer
 };
 
 export const sorters = {
   toMaxSorter,
-  toMinSorter,
+  toMinSorter
 };
